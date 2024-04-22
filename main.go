9bdfd508d3b581c3300590661a77fd03c8b520a5
VPeti1 <vpeti011@outlook.com>
2024-04-22 17:18:06 +0200

    V2

--- main.go
+++ main.go
@@ -8,31 +8,35 @@ import (
 )
 
 func main() {
-	// Ask for file path
-	fmt.Print("Enter the file path: ")
-	var filePath string
-	fmt.Scanln(&filePath)
-
-	// Open the file
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-
-	// Create SHA256 hash
-	hash := sha256.New()
-
-	// Copy the file content into the hash object
-	if _, err := io.Copy(hash, file); err != nil {
-		fmt.Println("Error reading file:", err)
+	// Check if at least one argument is provided
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: gohash file_path1 [file_path2 ...]")
 		return
 	}
 
-	// Calculate the hash
-	hashInBytes := hash.Sum(nil)
+	// Iterate over provided file paths
+	for _, filePath := range os.Args[1:] {
+		// Open the file
+		file, err := os.Open(filePath)
+		if err != nil {
+			fmt.Printf("Error opening file %s: %s\n", filePath, err)
+			continue
+		}
+		defer file.Close()
 
-	// Print the hash
-	fmt.Printf("SHA256 hash of %s: %x\n", filePath, hashInBytes)
+		// Create SHA256 hash
+		hash := sha256.New()
+
+		// Copy the file content into the hash object
+		if _, err := io.Copy(hash, file); err != nil {
+			fmt.Printf("Error reading file %s: %s\n", filePath, err)
+			continue
+		}
+
+		// Calculate the hash
+		hashInBytes := hash.Sum(nil)
+
+		// Print the hash
+		fmt.Printf("SHA256 hash of %s: %x\n", filePath, hashInBytes)
+	}
 }
